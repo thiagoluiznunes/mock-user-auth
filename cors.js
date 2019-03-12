0227016ca96726aba062599e6a5afedf11232506
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-03-11 21:53:33 -0300

    add cors

---
+++ cors.js
@@ -0,0 +1,12 @@
+module.exports = (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+};
