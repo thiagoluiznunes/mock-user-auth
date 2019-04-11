c596c42480a7bec5d7f2d72542a41b14ba151719
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-04-10 23:18:50 -0300

    create unit tests

---
+++ components/mock/mock.spec.js
@@ -0,0 +1,17 @@
+import chai from 'chai';
+import mock from './mock';
+import express from 'express';
+
+const assert = chai.assert;
+
+describe('Module mock' , () => {
+  it('It should be a module', () => {
+    assert.typeOf(mock, 'Object');
+  });
+});
+
+describe('Module mock api' , () => {
+  it('It should be a express router function ', () => {
+    assert.typeOf(mock.api, 'function');
+  });
+});
