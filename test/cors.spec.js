651694c0eef80ec90680cf7d1d4bb8eda743be02
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-06-01 14:13:35 -0300

    create cors unit tests

---
+++ test/cors.spec.js
@@ -0,0 +1,26 @@
+process.env.NODE_ENV = 'test';
+
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../bin/www';
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Cors middleware', () => {
+  it('It should return 200', () => {
+    chai.request(server)
+      .options('/api/v1/')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+      });
+  });
+  it('It should pass request', () => {
+    chai.request(server)
+      .get('/api/v1/')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(404);
+      });
+  });
+});
