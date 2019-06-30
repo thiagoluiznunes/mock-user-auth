f8c17699b92b0f4ad47aba885ca6b34986510a53
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-06-30 01:17:46 -0300

    update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,2 +1,3 @@
 #! /usr/local/bin/bash
 #git+https://github.com/thiagoluiznunes/mock-user-auth
+npm install --save @babel/cli~7.4.3 @babel/core~7.4.3 @babel/node~7.2.2 @babel/preset-env~7.4.3 babel-plugin-istanbul~5.1.2 babel-polyfill~6.26.0 body-parser~1.18.3 debug~~2.6.9 express~~4.16.0 faker~4.1.0 http-errors~~1.6.2 morgan~~1.9.0 nodemon~1.18.11 jsonwebtoken~8.5.1
